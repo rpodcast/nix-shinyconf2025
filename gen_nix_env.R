36cae129bbe609404360fd9eb0a6bb1165dc4682
Eric Nantz <theRcast@gmail.com>
2025-04-08 14:35:08 -0400

    feat: add new packages to nix config

--- gen_nix_env.R
+++ gen_nix_env.R
@@ -2,7 +2,14 @@ library(rix)
 
 rix(
   r_ver = "4.4.3",
-  r_pkgs = c("quarto"),
+  r_pkgs = c("quarto", "pkgnet", "pkggraph"),
+  # git_pkgs = list(
+  #   list(
+  #     package_name = "depgraph",
+  #     repo_url = "https://github.com/crsh/depgraph",
+  #     commit = "95b6158c6c8eb587e81b931dbbb55b5fe31e524e"
+  #   )
+  # ),
   system_pkgs = c("quarto"),
   ide = "none",
   overwrite = TRUE,
